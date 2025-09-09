f998f53131ce3235dc924df28db9ac9c6537c899
zSlayer <118687503+GwynnBleidd61@users.noreply.github.com>
2025-09-09 22:50:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
+import os
+from urllib.parse import unquote_plus
 from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def greet():
-    name = request.args.get("name", "Stranger")
-    message = request.args.get("message", "")
+    name_raw = request.args.get("name", "Stranger")
+    msg_raw  = request.args.get("message", "")
+    name = unquote_plus(name_raw)
+    message = unquote_plus(msg_raw)
     return f"Hello {name}! {message}"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
